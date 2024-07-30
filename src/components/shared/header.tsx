6c2e677807d568d7ac0f1bf8157e5eec76dde299
Maxnikit <max.nikit.03@gmail.com>
2024-07-30 03:07:55 +0300

    Add right part of header

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -2,6 +2,7 @@ import { Container } from "@/components/shared";
 import { Button } from "@/components/ui";
 
 import { cn } from "@/lib/utils";
+import { ArrowRight, ShoppingCart, User } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
@@ -26,8 +27,25 @@ export function Header({ className }: Props) {
 
         {/* Правая часть */}
         <div className="flex items-center gap-4">
-          <Button variant="outline">Войти</Button>
-          <Button>Регистрация</Button>
+          <Button variant="outline" className="flex items-center gap-1">
+            <User size={16} />
+            Войти
+          </Button>
+
+          <div>
+            <Button className="group relative">
+              <b>5220 ₽</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3" />
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart className=" relative" size={16} strokeWidth={2} />
+                <b>3</b>
+              </div>
+              <ArrowRight
+                size={20}
+                className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
+              />
+            </Button>
+          </div>
         </div>
       </Container>
     </header>
