8bd827cbf56cd25804bcd7ef033fd8062d426de9
Maxnikit <max.nikit.03@gmail.com>
2024-07-30 04:10:01 +0300

    Add price inputs to Filters

--- src/components/shared/filters.tsx
+++ src/components/shared/filters.tsx
@@ -1,4 +1,5 @@
-import { FilterCheckbox, Title } from "@/components/shared";
+import { FilterCheckbox, RangeSlider, Title } from "@/components/shared";
+import { Input } from "@/components/ui";
 import { cn } from "@/lib/utils";
 import React from "react";
 
@@ -14,6 +15,21 @@ export function Filters({ className }: Props) {
       <div className="flex flex-col gap-4">
         <FilterCheckbox text="Можно собирать" value="1" />
         <FilterCheckbox text="Нельзя собирать" value="2" />
+      </div>
+
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="mb-3 font-bold">Цена от и до:</p>
+        <div className="mb-5 flex gap-3">
+          <Input
+            type="number"
+            placeholder="0"
+            min={0}
+            max={1000}
+            defaultValue={0}
+          />
+          <Input type="number" placeholder="0" min={100} max={30000} />
+        </div>
+        <RangeSlider min={0} max={5000} step={100} value={[0, 5000]} />
       </div>
     </div>
   );
