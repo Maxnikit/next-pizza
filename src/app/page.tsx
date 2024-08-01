079cedba997279694850897b49d8c5e601b6d768
Maxnikit <max.nikit.03@gmail.com>
2024-08-02 01:50:50 +0300

    Add whitespace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,11 +28,13 @@ export default async function Home() {
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
+
       <TopBar
         categories={categories.filter(
           (category) => category.products.length > 0,
         )}
       />
+
       <Container className="mt-10 pb-14">
         <div className="flex gap-[80px]">
           {/* Фильтрация */}
