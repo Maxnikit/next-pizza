064bf0fe1091ba82ead494e571e2e2320d05d9b0
Maxnikit <max.nikit.03@gmail.com>
2024-08-03 20:48:47 +0300

    Change variable name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const nunito = Nunito({
   weight: ["400", "500", "600", "700", "800", "900"],
 });
 
-export default function RootLayout({
+export default function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
