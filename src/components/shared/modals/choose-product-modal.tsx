123d258516dca3234356841dc7f6fec4fea729fc
Maxnikit <max.nikit.03@gmail.com>
2024-08-03 16:08:54 +0300

    Add hidden Description and Title for modal

--- src/components/shared/modals/choose-product-modal.tsx
+++ src/components/shared/modals/choose-product-modal.tsx
@@ -2,9 +2,14 @@
 import { ChoosePizzaForm } from "@/components/shared/choose-pizza-form";
 import { Title } from "@/components/shared/title";
 import { Dialog } from "@/components/ui";
-import { DialogContent } from "@/components/ui/dialog";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
 import { Product } from "@prisma/client";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { useRouter } from "next/navigation";
 import React from "react";
 
@@ -20,10 +25,16 @@ export function ChooseProductModal({ product, className }: Props) {
     <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
       <DialogContent
         className={cn(
-          "max-w[1060px] min-h-[500px] w-[1060px] overflow-hidden bg-white p-0",
+          "min-h-[500px] w-[1060px] max-w-[1060px] overflow-hidden bg-white p-0",
           className,
         )}
       >
+        <VisuallyHidden>
+          <DialogTitle>{product.name}</DialogTitle>
+          <DialogDescription>
+            Choose a variant of your product
+          </DialogDescription>
+        </VisuallyHidden>
         <ChoosePizzaForm product={product} />
       </DialogContent>
     </Dialog>
