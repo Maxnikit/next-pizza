3239fa17102e02e01013b7ccacb1983aa6d52d8a
Maxnikit <max.nikit.03@gmail.com>
2024-08-18 19:04:06 +0300

    Add API call to get cartItems by token

---
+++ src/app/api/cart/route.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/prisma/prisma-client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const token = req.cookies.get("cartToken")?.value;
+
+    if (!token) {
+      return NextResponse.json({ items: [] });
+    }
+    const userCart = await prisma.cart.findFirst({
+      where: { token },
+      include: {
+        items: {
+          orderBy: { createdAt: "desc" },
+          include: {
+            productVariation: { include: { product: true } },
+            ingredients: true,
+          },
+        },
+      },
+    });
+    return NextResponse.json(userCart);
+  } catch (error) {
+    console.log(error);
+  }
+}
