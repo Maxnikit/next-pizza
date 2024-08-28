5fd40c72d60778996c7170f92ea159e79646e0fa
Maxnikit <max.nikit.03@gmail.com>
2024-08-29 01:47:58 +0300

    Start working on adding items to cart

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,5 +1,7 @@
 import { prisma } from "@/prisma/prisma-client";
 import { NextRequest, NextResponse } from "next/server";
+import crypto from "crypto";
+import { findOrCreateCart } from "@/shared/lib/find-or-create-cart";
 
 export async function GET(req: NextRequest) {
   try {
@@ -22,11 +24,37 @@ export async function GET(req: NextRequest) {
     });
     return NextResponse.json(userCart);
   } catch (error) {
-    console.log(error);
+    console.log("[CART_GET] Server error", error);
     return NextResponse.json(
-      { error: "An error occurred while fetching the cart" },
+      { error: "Не удалось получить корзину" },
       { status: 500 },
     );
   }
 }
 
+export async function POST(req: NextRequest) {
+  try {
+    let token = req.cookies.get("cartToken")?.value;
+
+    if (!token) {
+      token = crypto.randomUUID();
+    }
+
+    const userCart = await findOrCreateCart(token);
+
+    const findCartItem = await prisma.cartItem.findFirst({
+      where: {
+        cartId: userCart.id,
+        // TODO keep working
+      },
+    });
+
+    return NextResponse.json(userCart);
+  } catch (error) {
+    console.log("[CART_POST] Server error", error);
+    return NextResponse.json(
+      { error: "Не удалось создать корзину" },
+      { status: 500 },
+    );
+  }
+}
